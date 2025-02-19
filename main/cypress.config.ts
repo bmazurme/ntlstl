0e08ef7823055d89ac3fb82ed7de3dc0e8915770
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-02-19 20:14:28 +0300

    feat: main cypress

---
+++ main/cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress';
+import { config as dotEnvConfig } from 'dotenv';
+
+dotEnvConfig();
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000/',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      config.env = {
+        ...process.env,
+        ...config.env,
+      };
+
+      return config;
+    },
+  },
+});
